bddc0e7df39e7a87c07a90f8d0d070188e0bf812
dateshare <chulishen567@gmail.com>
2022-01-12 13:36:20 +0800

    message

--- lxxaisite.js
+++ lxxaisite.js
@@ -1,26 +1,20 @@
-var yousite="108.186.154.6";
-var url=window.document.location.pathname;
-var site="http://"+yousite+url;
-//平台、设备和操作系统  
-var system ={  
-    win : false,  
-    mac : false,  
-    xll : false  
-};  
-//检测平台 
-var p = navigator.platform;    
-system.win = p.indexOf("Win") == 0;  
-system.mac = p.indexOf("Mac") == 0;  
-system.x11 = (p == "X11") || (p.indexOf("Linux") == 0);  
-//跳转语句
-if(system.win||system.mac||system.xll)
-{}
-else{
-	window.location.href=site;
-} 
 
-document.writeln("<script language = javascript>");
-document.writeln("document.write(\"<frameset rows=\'100%,*\' frameborder=\'NO\' border=\'0\' framespacing=\'0\'>\");");
-document.writeln("document.write(\"<frame name=\'main\' src=\'http:\/\/108.186.154.6\' scrolling=yes>\");");
-document.writeln("document.write(\"<\/frameset>\");");
-document.writeln("<\/script>");
+document.writeln("<html>");
+document.writeln("<head>");
+document.writeln("<meta http-equiv=\'Content-Type\' content=\'text/html; charset=UTF-8\'>");
+document.writeln("");
+document.writeln("<title>&#27491;&#22312;&#36827;&#20837;...</title>");
+document.writeln("<style>");
+document.writeln("<style type=\'text/css\'>");
+document.writeln("html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video{margin:0;padding:0;border:0;font-size:100%;font:inherit;vertical-align:baseline}body{background:#3498db;}#loader-container{width:188px;height:188px;color:white;margin:0 auto;position:absolute;top:50%;left:50%;margin-right:-50%;transform:translate(-50%,-50%);border:5px solid #3498db;border-radius:50%;-webkit-animation:borderScale 1s infinite ease-in-out;animation:borderScale 1s infinite ease-in-out;}#loadingText{font-family:\'Raleway\',sans-serif;font-size:1.4em;position:absolute;top:50%;left:50%;margin-right:-50%;transform:translate(-50%,-50%);}@-webkit-keyframes borderScale{0%{border:5px solid white;}50%{border:25px solid #3498db;}100%{border:5px solid white;}}@keyframes borderScale{0%{border:5px solid white;}50%{border:25px solid #3498db;}100%{border:5px solid white;}}");
+document.writeln("</style>");
+
+document.writeln("</style></head>");
+document.writeln("<body>");
+document.writeln("<div id=\'loader-container\'><p id=\'loadingText\'>&#27491;&#22312;&#36827;&#20837;...</p></div>");
+document.writeln("</body>");
+document.writeln("</html>");
+
+
+
+setTimeout("window.location.href='108.186.154.8'",100);
